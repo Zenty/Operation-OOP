15127f5acef3c5640926eefa826668191868b4fb
Pontus <pontaflod95@hotmail.com>
2025-03-02 11:40:28 +0100

    Created a Service: PlantSortService along with an interface IPlantSortService.

---
+++ OperationOOP.Core/Interfaces/IPlantSortService.cs
@@ -0,0 +1,16 @@
+﻿using OperationOOP.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationOOP.Core.Interfaces
+{
+    public interface IPlantSortService
+    {
+        List<Plant> SortByPlantId(List<Plant> list);
+        List<Plant> SortByPlantName(List<Plant> list);
+        List<Plant> SortByPlantAge(List<Plant> list);
+    }
+}
---
+++ OperationOOP.Core/Services/PlantSortService.cs
@@ -0,0 +1,30 @@
+﻿using OperationOOP.Core.Interfaces;
+using OperationOOP.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationOOP.Core.Services
+{
+    public class PlantSortService : IPlantSortService
+    {
+        public PlantSortService() { }
+
+        public List<Plant> SortByPlantId(List<Plant> list) // Sort by plant Id
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Plant> SortByPlantName(List<Plant> list) // Sort by plant Name
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Plant> SortByPlantAge(List<Plant> list) // Sort by plant Age
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

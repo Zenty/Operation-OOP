d604806e481c1180ee69c03e68feba910a4ca352
Pontus <pontaflod95@hotmail.com>
2025-03-04 12:21:15 +0100

    Removed my Interface & Service as they were redundent since I moved the Sort directly into my EndPoints.

---
+++ OperationOOP.Api/Endpoints/Sorted/GetSortedByCareLevel.cs
@@ -0,0 +1,36 @@
+﻿namespace OperationOOP.Api.Endpoints;
+
+public class GetSortedByCareLevel: IEndpoint
+{
+    // Mapping
+    public static void MapEndpoint(IEndpointRouteBuilder app) => app
+        .MapGet("/plants/sort/carelevel", Handle)
+        .WithSummary("Plants sorted by CareLevel");
+
+    // Request and Response types
+    public record Response(
+        int Id,
+        string Name,
+        string Species,
+        int AgeYears,
+        DateTime LastWatered,
+        DateTime LastPruned,
+        CareLevel CareLevel
+    );
+
+    //Logic
+    private static List<Response> Handle(IDatabase db)
+    {
+        return [.. db.Plants
+            .Select(item => new Response(
+                Id: item.Id,
+                Name: item.Name,
+                Species: item.Species,
+                AgeYears: item.AgeYears,
+                LastWatered: item.LastWatered,
+                LastPruned: item.LastPruned,
+                CareLevel: item.CareLevel
+            ))
+            .OrderBy(p => p.CareLevel)];
+    }
+}
